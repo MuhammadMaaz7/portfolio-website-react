b7b253867fa4637646260438db0dd95209502eb9
MuhammadMaaz7 <maazawan100@gmail.com>
2025-06-12 21:58:45 +0500

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from '@vercel/analytics';
 import App from './App.tsx';
 import './index.css';
 
 injectSpeedInsights();
+inject();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
